a94dab8291edd949d1a32a59a23a0eaeb8a9cd0a
rtrppl <63909884+rtrppl@users.noreply.github.com>
2025-01-27 20:25:23 +0100

    0.5.2
    Added options to manually set cleandesk-data-folders and to specify location of the dotfile; improved documentation

--- cleandesk.el
+++ cleandesk.el
@@ -4,7 +4,7 @@
 
 ;; Maintainer: René Trappel <rtrappel@gmail.com>
 ;; URL: https://github.com/rtrppl/cleandesk
-;; Version: 0.5.1
+;; Version: 0.5.2
 ;; Package-Requires: ((emacs "26"))
 ;; Keywords: files
 
@@ -38,9 +38,21 @@
 (defvar cleandesk-metadata-tool "fd") ;; choose between find and fd; fd is much faster and standard
 (defvar cleandesk-fd-metadata-cmd "fd -t d --no-hidden .")
 (defvar cleandesk-find-metadata-cmd "-type d ! -name '.*' | sed 's@//@/@'")
-(defvar cleandesk-folders nil "All folders cleandesk operates on.")
+(defvar cleandesk-folders nil "A list with all folders cleandesk operates on.
+This is a temporay variable used (and changed by most functions). It is not 
+intended for the user to modify! If you want to specfiy folders cleandesk 
+should operate on, please modify `cleandesk-data-folders'")
 (defvar cleandesk-name-directory (make-hash-table :test 'equal))
 (defvar cleandesk-data-folders nil "Includes all folders that cleandesk should opperate on (excluding subfolders).")
+;; You do not need to set `cleandesk-data-folders' to begin working with 
+;; cleandesk. If set to nil, this variable is modified by `cleandesk-add-folder' and
+;; `cleandesk-remove-folder'. If this is set to any value other than nil,
+;; these functions do not change `cleandesk-data-folders'.
+(defvar cleandesk-dotfile "~/.cleandesk-directory-list")
+;; The dotfile stores all `cleandesk-data-folders' and should only be 
+;; modified via `cleandesk-add-folder' and `cleandesk-remove-folder'.
+;; If `cleandesk-data-folders' are manually set, this variable can be
+;; set to nil.
 (defvar cleandesk-fd4tree-cmd "fd -t d --no-ignore -L .")
 (defvar cleandesk-search-tool "mdfind") ;; choose between mdfind (macOS only), rg (text documents only) and rga (slower)
 (defvar cleandesk-tree-cmd "tree -d -f -R ")
@@ -51,19 +63,28 @@
   (eq system-type 'darwin))
 
 (defun cleandesk-prepare-folder-list ()
- "Preparing a list of all folders in the cleandesk folders for further tasks."
- (cleandesk-get-folder-list)
- (setq cleandesk-folders "")
- (setq cleandesk-data-folders (hash-table-values cleandesk-name-directory))
-   (dolist (cleandesk-data-folder cleandesk-data-folders)
+ "Preparing a list of all sub-folders in `cleandesk-data-folders'."
+ (let ((cleandesk-working-folders))
+   (setq cleandesk-folders "")
+   (when (not cleandesk-data-folders)
+     (cleandesk-get-folder-list)
+     (setq cleandesk-working-folders (hash-table-values cleandesk-name-directory)))
+   (when cleandesk-data-folders
+     (dolist (folder cleandesk-data-folders)
+       (setq cleandesk-working-folders (append (list (expand-file-name folder)) cleandesk-working-folders))))
+   (dolist (cleandesk-data-folder cleandesk-working-folders)
      (with-temp-buffer
        (when (string-equal cleandesk-metadata-tool "fd")
-	 (insert (shell-command-to-string (concat cleandesk-fd-metadata-cmd " '" cleandesk-data-folder "' "))))
+	 (insert (shell-command-to-string (concat cleandesk-fd-metadata-cmd " \""  cleandesk-data-folder "\" "))))
        (when (string-equal cleandesk-metadata-tool "find")
 	 (insert (shell-command-to-string (concat "find " cleandesk-data-folder " " cleandesk-find-metadata-cmd))))
        (let ((temp-folders (split-string (buffer-string) "\n" t)))
 	 (setq cleandesk-folders (append temp-folders cleandesk-folders)))))
-	 (setq cleandesk-folders (append cleandesk-data-folders cleandesk-folders)))
+   (when (not cleandesk-data-folders) 
+     (setq cleandesk-folders (append cleandesk-working-folders cleandesk-folders)))
+   (when cleandesk-data-folders
+     (dolist (folder cleandesk-data-folders)
+       (setq cleandesk-folders (append (list (expand-file-name folder)) cleandesk-folders))))))
 
 (defun cleandesk-jump-to-folder ()
   "Selects a folder from the list of cleandesk folders and opens a dired view."
@@ -167,7 +188,7 @@
  "Checks for Cleandesk file ~/.cleandesk-directory-list. It directs the user 
 to cleandesk-add-folder in case there is none."
  (clrhash cleandesk-name-directory)
- (when (not (file-exists-p "~/.cleandesk-directory-list"))
+ (when (not (file-exists-p cleandesk-dotfile))
    (read-char "You need to add at least one folder as a Cleandesk folder. Press any key to proceed.")
    (cleandesk-add-folder)))
 
@@ -178,54 +199,55 @@ Cleandesk considers."
  (clrhash cleandesk-name-directory)
  (cleandesk-check-for-cd-file)
  (with-temp-buffer
-   (insert-file-contents "~/.cleandesk-directory-list")
+   (insert-file-contents cleandesk-dotfile)
    (if (fboundp 'json-parse-buffer)
        (setq cleandesk-name-directory (json-parse-buffer)))))
 
 (defun cleandesk-add-folder ()
   "Add a directory to the list of Cleandesk directories."
   (interactive)
-  (clrhash cleandesk-name-directory)
-  (let* ((new-directory (expand-file-name (read-directory-name "Enter a directory name: ")))
-	 (name (read-from-minibuffer "Please provide a name for the new Cleandesk directory: ")))
-    (if (yes-or-no-p (format "Are you sure you want to add %s as a new Cleandesk directory? " new-directory))
-	(progn
-	  (unless (file-exists-p new-directory)
-	    (make-directory new-directory t))
-	  (when (not (file-exists-p "~/.cleandesk-directory-list"))
-	    (puthash name new-directory cleandesk-name-directory))
-	  (when (file-exists-p "~/.cleandesk-directory-list")
-	    (with-temp-buffer
-	      (insert-file-contents "~/.cleandesk-directory-list")
-	      (if (fboundp 'json-parse-buffer)
-		  (setq cleandesk-name-directory (json-parse-buffer))))
-	    (puthash name new-directory cleandesk-name-directory))
-	    (with-temp-buffer
-	     (let* ((json-data (json-encode cleandesk-name-directory)))
-	       (insert json-data)
-	       (write-file "~/.cleandesk-directory-list"))))
-    (message "%s was not created!" new-directory))
+    (when (not cleandesk-data-folders)
+      (clrhash cleandesk-name-directory)
+      (let* ((new-directory (expand-file-name (read-directory-name "Enter a directory name: ")))
+	     (name (read-from-minibuffer "Please provide a name for the new Cleandesk directory: ")))
+      (when (not (file-exists-p cleandesk-dotfile))
+	(puthash name new-directory cleandesk-name-directory))
+      (when (file-exists-p cleandesk-dotfile)
+	(with-temp-buffer
+	  (insert-file-contents cleandesk-dotfile)
+	  (if (fboundp 'json-parse-buffer)
+	      (setq cleandesk-name-directory (json-parse-buffer))))
+	(puthash name new-directory cleandesk-name-directory))
+      (with-temp-buffer
+	(let* ((json-data (json-encode cleandesk-name-directory)))
+	  (insert json-data)
+	  (write-file cleandesk-dotfile)))))
+  (when cleandesk-data-folders
+    (message "cleandesk-add-folder does not work when cleandesk-data-folders has been set manually.")
   (clrhash cleandesk-name-directory)))
 
 (defun cleandesk-remove-folder ()
   "Remove a directory from the list of Cleandesk directories."
   (interactive)
-  (cleandesk-get-folder-list)
-  (let* ((directories (hash-table-keys cleandesk-name-directory))
-	(selection (completing-read "Which cleandesk directory should be removed? " directories)))
-    (if (not (member selection directories))
-	(message "Directory does not exist.")
-      (if (yes-or-no-p (format "Are you sure you want to remove %s as a cleandesk directory? " (gethash selection cleandesk-name-directory)))
-	  (progn
-	    (remhash selection cleandesk-name-directory)
-	    (when (not (eq (hash-table-count cleandesk-name-directory) 0)) 
-	      (with-temp-buffer
-		(let ((json-data (json-encode cleandesk-name-directory)))
-		      (insert json-data))
-		      (write-file "~/.cleandesk-directory-list")))
-	    (when (eq (hash-table-count cleandesk-name-directory) 0)
-	      (delete-file "~/.cleandesk-directory-list")))
-	(clrhash cleandesk-name-directory)))))
+   (when (not cleandesk-data-folders)
+     (cleandesk-get-folder-list)
+     (let* ((directories (hash-table-keys cleandesk-name-directory))
+	    (selection (completing-read "Which cleandesk directory should be removed? " directories)))
+       (if (not (member selection directories))
+	   (message "Directory does not exist.")
+	 (if (yes-or-no-p (format "Are you sure you want to remove %s as a cleandesk directory? " (gethash selection cleandesk-name-directory)))
+	     (progn
+	       (remhash selection cleandesk-name-directory)
+	       (when (not (eq (hash-table-count cleandesk-name-directory) 0)) 
+		 (with-temp-buffer
+		   (let ((json-data (json-encode cleandesk-name-directory)))
+		     (insert json-data))
+		   (write-file cleandesk-dotfile)))
+	       (when (eq (hash-table-count cleandesk-name-directory) 0)
+		 (delete-file cleandesk-dotfile)))
+	   (clrhash cleandesk-name-directory)))))
+   (when cleandesk-data-folders
+    (message "cleandesk-remove-folder does not work when cleandesk-data-folders has been set manually.")))
 
 (defun cleandesk-search (arg)
   "Search for all files containing a specific string in the current directory. 
